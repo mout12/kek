cd91363d83b1e7da5ed56bb7b4a2d10b7717831f
mout12 <mout12@gmail.com>
2018-02-21 20:21:09 -0800

    added a message

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("my name is jackson");
+            Console.WriteLine("Hey dude what are you up to ? ");
         }
     }
 }
